b64a099dfe4e2aba921228994f9a469332a39be5
Matheus MN. Nicolielo <mnicolielo@atakone.com.br>
2020-01-09 17:14:48 -0300

    Refazendo curso - aula 1

--- AppsFlutter/RefazendoTodoList/refazendotodolist/lib/main.dart
+++ AppsFlutter/RefazendoTodoList/refazendotodolist/lib/main.dart
@@ -1,45 +1,25 @@
 import 'package:flutter/material.dart';
 import 'models/items.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(App());
 
-class MyApp extends StatelessWidget {
+class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Todo List',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.blue, // Paleta de Cores
       ),
       home: HomePage(),
     );
   }
 }
 
-class HomePage extends StatefulWidget {
-  var items = new List<Item>();
-
-  HomePage() {
-    items = [];
-  }
-
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Teste"),
-      ),
-      body: ListView.builder(
-          itemCount: widget.items.length,
-          itemBuilder: (BuildContext ctxt, int index) {
-            final item = widget.items[index];
-          }),
-    );
+    return Container();
   }
 }
