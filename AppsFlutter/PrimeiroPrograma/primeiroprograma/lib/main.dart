693930174a831f9e0f1e89f66d918cf89a945661
Matheus MN. Nicolielo <mnicolielo@atakone.com.br>
2020-01-09 15:16:59 -0300

    Terminando TodoList, iniciando refazer

--- AppsFlutter/PrimeiroPrograma/primeiroprograma/lib/main.dart
+++ AppsFlutter/PrimeiroPrograma/primeiroprograma/lib/main.dart
@@ -1,5 +1,6 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
-
+import 'package:shared_preferences/shared_preferences.dart';
 import 'models/item.dart';
 
 void main() => runApp(App());
@@ -23,9 +24,6 @@ class HomePage extends StatefulWidget {
 
   HomePage() {
     items = [];
-    items.add(Item(title: "Banana", done: false));
-    items.add(Item(title: "Abacate", done: true));
-    items.add(Item(title: "Laranja", done: false));
   }
 
   @override
@@ -42,7 +40,37 @@ class _HomePageState extends State<HomePage> {
         Item(title: newTaskCtrl.text, done: false),
       );
       newTaskCtrl.text = ""; // ou newTaskCtrl.clear();
+      save();
     });
+  }
+
+  void remove(int index) {
+    setState(() {
+      widget.items.removeAt(index);
+      save();
+    });
+  }
+
+  Future load() async {
+    var prefs = await SharedPreferences.getInstance();
+    var data = prefs.getString('data');
+
+    if (data != null) {
+      Iterable decoded = jsonDecode(data);
+      List<Item> result = decoded.map((x) => Item.fromJson(x)).toList();
+      setState(() {
+        widget.items = result;
+      });
+    }
+  }
+
+  save() async {
+    var prefs = await SharedPreferences.getInstance();
+    await prefs.setString('data', jsonEncode(widget.items));
+  }
+
+  _HomePageState() {
+    load();
   }
 
   @override
@@ -80,10 +108,17 @@ class _HomePageState extends State<HomePage> {
               onChanged: (value) {
                 setState(() {
                   item.done = value;
+                  save();
                 });
               },
             ),
             key: Key(item.title),
+            background: Container(
+              color: Colors.red.withOpacity(0.5),
+            ),
+            onDismissed: (direction) {
+              remove(index);
+            },
           );
         },
       ),
