d70c8717ed71ca8d79d87095dd685ae77286b1da
Matheus MN. Nicolielo <mnicolielo@atakone.com.br>
2020-01-09 14:15:59 -0300

    primeiro programa, to do list

---
+++ AppsFlutter/PrimeiroPrograma/primeiroprograma/Anotacoes_2-0.md
@@ -0,0 +1,5 @@
+- **Swatch**  é a paleta de cores, quando usado somente o **colors** é uma cor única, agora **colors** com **swatch** trás toda a paleta da cor.
+
+- **Scaffold**: Widget que representa uma página.
+
+- **JSON TO DART** : https://javiercbk.github.io/json_to_dart/
--- AppsFlutter/PrimeiroPrograma/primeiroprograma/lib/main.dart
+++ AppsFlutter/PrimeiroPrograma/primeiroprograma/lib/main.dart
@@ -1,16 +1,99 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+import 'models/item.dart';
 
-class MyApp extends StatelessWidget {
+void main() => runApp(App());
+
+class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Primeiro Programa',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.green,
+        primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Home Page'),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  var items = new List<Item>();
+
+  HomePage() {
+    items = [];
+    items.add(Item(title: "Banana", done: false));
+    items.add(Item(title: "Abacate", done: true));
+    items.add(Item(title: "Laranja", done: false));
+  }
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var newTaskCtrl = TextEditingController();
+
+  void add() {
+    if (newTaskCtrl.text.isEmpty) return;
+    setState(() {
+      widget.items.add(
+        Item(title: newTaskCtrl.text, done: false),
+      );
+      newTaskCtrl.text = ""; // ou newTaskCtrl.clear();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        //leading: Text("Oi"), // Lugar do menu hambuguer
+        title: TextFormField(
+          controller: newTaskCtrl,
+          keyboardType: TextInputType.text,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 24,
+          ),
+          decoration: InputDecoration(
+            labelText: "Nova Tarefa",
+            labelStyle: TextStyle(
+              color: Colors.white,
+              fontSize: 18,
+            ),
+          ),
+        ),
+        /*actions: <Widget>[
+          Icon(Icons.plus_one), // Icones no canto superior direito
+        ],*/
+      ),
+      body: ListView.builder(
+        itemCount: widget.items.length,
+        itemBuilder: (BuildContext ctxt, int index) {
+          final item = widget.items[index];
+          return Dismissible(
+            child: CheckboxListTile(
+              title: Text(item.title),
+              value: item.done,
+              onChanged: (value) {
+                setState(() {
+                  item.done = value;
+                });
+              },
+            ),
+            key: Key(item.title),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          add();
+        },
+        child: Icon(Icons.add),
+        backgroundColor: Colors.pink,
+      ),
     );
   }
 }
